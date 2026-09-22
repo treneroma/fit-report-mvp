d2a2af6c2826844fe70b83cf75d4a7c5a6935382
treneroma <rablyasov@gmail.com>
2026-09-22 23:19:41 +0300

    Update athlete.js

--- screens/athlete.js
+++ screens/athlete.js
@@ -1824,8 +1824,6 @@ async function athleteMeasurementsRequest(action, extra = {}) {
   return result;
 }
 async function athleteSaveMeasurements() {
-    showMessage("Кнопка сработала");
-  return;
   const form = document.getElementById("athleteMeasurementsForm");
   const dateInput = document.getElementById("athleteMeasurementDate");
 
